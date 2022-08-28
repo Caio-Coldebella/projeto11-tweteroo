07dd0adc948c7debb29e8423076d3a591af80ae6
Caio-Coldebella <c232621@dac.unicamp.br>
2022-08-28 11:54:58 -0300

    Rota get tweets, todos os requisitos obrigatórios entregues

--- src/app.js
+++ src/app.js
@@ -9,15 +9,29 @@ const users = [];
 const tweets = [];
 
 app.post('/sign-up',(req,res) =>{
-    users.push(req.body)
-    res.send("OK")
-    console.log(users)
+    users.push(req.body);
+    res.send("OK");
 });
 
 app.post('/tweets', (req,res) => {
-    tweets.post(req.body)
-    res.send("OK")
-    console.log(tweets)
-})
+    const obj = req.body;
+    const url = users.find(usuario => usuario.username === obj.username).avatar;
+    obj.avatar = url;
+    tweets.push(obj);
+    res.send("OK");
+});
+
+app.get('/tweets', (req,res) => {
+    const last10 = [];
+    let size = tweets.length - 1;
+    let limit = -1;
+    if(size > 9){
+        limit = size - 10;
+    }
+    for(let i=size; i > limit ; i--){
+        last10.push(tweets[i]);
+    }
+    res.send(last10)
+});
 
 app.listen(5000);
