2e1e33f9db5c0c58cf4d66b46a6d27c6533d5fd9
Caio-Coldebella <c232621@dac.unicamp.br>
2022-08-28 11:27:54 -0300

    Rota post tweets

--- src/app.js
+++ src/app.js
@@ -6,10 +6,18 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post('/sign-up',(req,res) =>{
     users.push(req.body)
     res.send("OK")
+    console.log(users)
+});
+
+app.post('/tweets', (req,res) => {
+    tweets.post(req.body)
+    res.send("OK")
+    console.log(tweets)
 })
 
 app.listen(5000);
